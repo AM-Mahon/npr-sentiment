a12104f46cf05f375d935e076562b6ba223b393e
Ann Marie Mahon <Annmarie.Mahon@colorado.edu>
2021-05-08 20:25:50 -0600

    Add web app front end

---
+++ index.js
@@ -0,0 +1,48 @@
+require('dotenv').config();
+const  express = require('express');
+const { Client } = require('pg');
+const pgClient = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
+});
+var cors = require('cors');
+const path = require('path');
+
+const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {rejectUnauthorized: false}
+});
+
+client.connect();
+
+var app = express();
+
+app.use(cors());
+
+const retrieveArticles = async function(){
+    let articles = [];
+    const res = await client.query("SELECT * FROM articles WHERE sentiment IN (-1,0,1);", []);
+    for(let row of res.rows){
+        articles.push(row);
+    }
+    return articles;
+}
+
+var server = app.listen(process.env.port || 8080, function() {
+    console.log(`App now running on port ${server.address().port}`)
+    
+    app.get("/", function(req, res, next){
+        res.sendFile(path.join(__dirname+'/index.html'))
+    })
+
+    app.get("/css", function(req, res, next){
+        res.sendFile(path.join(__dirname+'/style.css'))
+    })
+
+    app.get("/articles", function(req, res, next){
+        retrieveArticles()
+        .then(articles => {
+            res.json(articles);
+        })
+    })
+})
